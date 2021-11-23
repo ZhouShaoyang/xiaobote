247b913ea73adb3bb65c98c672ea443fe1c3f14b
zhoushaoyang <zhoushaoyang@pku.edu.cn>
2021-11-23 19:21:29 +0800

    xiaobote

--- launcher.py
+++ launcher.py
@@ -5,11 +5,14 @@ import sys
 
 sys.path.append(os.getcwd())
 
-import traceback
+import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 from util.logger import Logger
+from config import page
+
+sys.path.append(os.getcwd())
 
 log = Logger()
 
@@ -21,16 +24,30 @@ class Browser(object):
         self.options.add_experimental_option('excludeSwitches', ['enable-automation'])
         self.browser = webdriver.Chrome(options=self.options)
 
-    def open(self, page, module):
+    def open(self, page):
         try:
             self.browser.get(url=page)
-            log.info(message=f'open {module} {page}')
+            log.info(message=f'open {page}')
         except Exception as err:
-            log.error(message=f'open {module} {page} {err}')
+            log.error(message=f'ope {page} {err}')
 
 
 if __name__ == '__main__':
-    options = Options()
-
-    browser = webdriver.Chrome(options=options)
-    browser.get('file:///home/pi/xiaobote/html/fake.html')
+    browser = Browser()
+    browser.open(page.month_all)
+    time.sleep(6)
+    browser.open(page.month_opt)
+    time.sleep(6)
+    browser.open(page.essay_entry)
+    time.sleep(3)
+    browser.open(page.essay_result)
+    time.sleep(6)
+    browser.open(page.bv_entry)
+    time.sleep(3)
+    browser.open(page.bv_result)
+    time.sleep(6)
+    browser.open(page.post_entry)
+    time.sleep(3)
+    browser.open(page.post_result)
+    time.sleep(6)
+    browser.open(page.week_all)
