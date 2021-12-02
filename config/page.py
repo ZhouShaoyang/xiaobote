99990e81739b110e8ddbb41cc44c289935c3024a
zhoushaoyang <zhoushaoyang@pku.edu.cn>
2021-12-02 18:23:36 +0800

    修改fake页面

--- config/page.py
+++ config/page.py
@@ -25,9 +25,9 @@ if mode == 'FAKE':
     essay = 'file:///home/pi/xiaobote/html/fake/essay.html'
     essay_result = 'file:///home/pi/xiaobote/html/fake/essay_result.html'
 
-    demo_essay_lbxygsn = 'http://essays.xiaobote.com/#/article?title=%E8%93%9D%E6%A0%87%E4%B8%8B%E4%B8%80%E4%B8%AA%E5%8D%81%E5%B9%B4&id=61a07d58899e0114e9cb4b44'
-    demo_essay_qtyyzsd = 'http://essays.xiaobote.com/#/article?title=%E5%80%BE%E5%90%AC%E5%85%83%E5%AE%87%E5%AE%99%E6%97%B6%E4%BB%A3&id=61a07c8b899e0114e9cb4b41'
-    demo_essay_qselzn = 'http://essays.xiaobote.com/#/article?title=%E6%81%B0%E4%BC%BC%E8%80%8C%E7%AB%8B%E4%B9%8B%E5%B9%B4&id=61a07ce4899e0114e9cb4b42'
+    demo_essay_lbxygsn = 'http://essays.xiaobote.com/#/article?&id=61a07d58899e0114e9cb4b44'
+    demo_essay_qtyyzsd = 'http://essays.xiaobote.com/#/article?id=61a07c8b899e0114e9cb4b41'
+    demo_essay_qselzn = 'http://essays.xiaobote.com/#/article?id=61a07ce4899e0114e9cb4b42'
 
     essay_api = 'http://www.baidu.com'
     poster_api = 'http://www.baidu.com'
